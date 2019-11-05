f1fc2fd986301fd0407dfaa0fc2f3d24d85d4cf6
itroger <743163143@qq.com>
2019-11-05 18:06:57 +0800

    更新

---
+++ WebSite/database/website.sh
@@ -0,0 +1,13 @@
+db.website.insertMany([{
+id: 10001,
+url: "https://html.spec.whatwg.org/",
+image: "../images/website_images/HTML5.svg",
+title: "HTML5",
+describe: "超文本标记语言"
+}, {
+id: 10002,
+url: "https://www.w3.org/Style/CSS/current-work",
+image: "../images/website_images/CSS31.svg",
+title: "CSS3",
+describe: "层叠样式表"
+}])
